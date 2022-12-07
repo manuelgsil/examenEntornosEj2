0f376887639eef62a8891cfe62ca6aa8a75ed74f
manuelgsil <118306123+manuelgsil@users.noreply.github.com>
2022-12-07 20:07:14 +0100

    Actualizacion de nombre

--- examen_ejercicio2/src/examen_ejercicio2/Main.java
+++ examen_ejercicio2/src/examen_ejercicio2/Main.java
@@ -3,7 +3,7 @@ package examen_ejercicio2;
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("Hola me llamo");
+		System.out.println("Hola me llamo Manuel");
 
 	}
 
